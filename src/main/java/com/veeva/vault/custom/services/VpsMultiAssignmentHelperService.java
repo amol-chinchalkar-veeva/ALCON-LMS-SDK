0fcb5aaf3d767a7faccdeea96b2abdf57328158b
Amol Chinchalkar <amol.chinchalkar@veeva.com>
2022-07-19 18:45:35 -0400

    Restructure Code

--- src/main/java/com/veeva/vault/custom/services/VpsMultiAssignmentHelperService.java
+++ src/main/java/com/veeva/vault/custom/services/VpsMultiAssignmentHelperService.java
@@ -14,17 +14,19 @@
  */
 package com.veeva.vault.custom.services;
 
-import com.veeva.vault.custom.model.VpsExternalTrainingModel;
+import com.veeva.vault.custom.model.VpsExternalTrainingInfo;
 import com.veeva.vault.sdk.api.core.*;
-import com.veeva.vault.sdk.api.data.Record;
 
 import java.util.List;
 import java.util.Map;
 
 @UserDefinedServiceInfo
-public interface VpsExternalTrainingHelperService extends UserDefinedService {
-   Map<String, List<VpsExternalTrainingModel>> getExternalTrainingAssignmentModels(String id);
-   Map<List<String>, List<String>> createFacilitatedTrainingForAssignments(Map<String, List<VpsExternalTrainingModel>> vpsExternalTrainingQueueItemsMap);
-   List<String>  createFacilitatedReqAndTrainingAssignment(Map<List<String>, List<String>> facilitatedTrainingIDsAsMap);
-   void callUserAction(Map<List<String>, List<String>> facilitatedTrainingIDsAsMap, String actionName);
+public interface VpsMultiAssignmentHelperService extends UserDefinedService {
+    Map<String, List<VpsExternalTrainingInfo>> getExternalTrainingAssignmentInfo(String id, String trainingType);
+
+    Map<List<String>, List<String>> createFacilitatedTrainingForAssignments(Map<String, List<VpsExternalTrainingInfo>> vpsExternalTrainingQueueItemsMap);
+
+    List<String> addTrainingAssignmentsInFacilitiatedTraining(Map<List<String>, List<String>> facilitatedTrainingIDsAsMap);
+
+    void completeFacilitatedTrainingRequest(Map<List<String>, List<String>> facilitatedTrainingIDsAsMap, String actionName);
 }
